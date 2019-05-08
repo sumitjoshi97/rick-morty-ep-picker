34e9c103ddd022a7f485792463f4b3186064238c
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-05-08 17:47:16 +0530

    chore: fetch episodes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,52 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Store } from './Store'
 
+interface IEpisode {
+  id: number
+  airdate: string
+  airstamp: string
+  airtime: string
+  name: string
+  image: { medium: string; original: string }
+  season: number
+  number: number
+  runtime: number
+  summary: string
+}
+
 const App: React.FC = () => {
-  const store = React.useContext(Store)
+  const { state, dispatch } = useContext(Store)
+
+  useEffect(() => {
+    state.episodes.length === 0 && FetchDataAction()
+  })
+
+  const FetchDataAction = async () => {
+    const URL =
+      'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes'
+    const data = await fetch(URL)
+    const dataJSON = await data.json()
+    return dispatch({
+      type: 'FETCH_DATA',
+      payload: dataJSON._embedded.episodes,
+    })
+  }
+
   return (
     <>
       <h1>Rick and Morty</h1>
       <p>Pick your favorite episodes</p>
+      <section>
+        {state.episodes.map((episode: IEpisode) => (
+          <section key={episode.id}>
+            <div>{episode.name}</div>
+            <section>
+              <img src={episode.image.medium} />
+              Season: {episode.season} Number: {episode.number}
+            </section>
+          </section>
+        ))}
+      </section>
     </>
   )
 }
